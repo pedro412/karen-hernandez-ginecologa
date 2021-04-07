a8062efbca4489f3f972cf1fdb105c369348e0bd
pedro412 <pedro.alvarez412@outlook.com>
2021-04-07 14:33:13 -0500

    remove hosting

--- public/app.js
+++
@@ -1,9 +0,0 @@
-const form = () => {
-  fetch('https://us-central1-vimind-3526e.cloudfunctions.net/app')
-    .then((resp) => resp.json())
-    .then((resp) => {
-      console.log(resp);
-    });
-};
-
-form();
